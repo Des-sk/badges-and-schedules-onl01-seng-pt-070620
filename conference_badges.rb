353ab53453ef2a726848d9fd2a5480eab165ceb7
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-14 21:26:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,5 +24,8 @@ def printer(attendees)
   badges_and_room_assignments.each_line do |line|
     badges_and_room_assignments<< ("Hello, my name is #{name}")
     rooms<< ("Hello, #{name}! You'll be assigned to room #{index+1}!")
+  end
+  printer
+end
 
 
