42d99038817d58d878317e3096b536192d8d7fa7
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-14 21:45:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,10 +20,10 @@ def assign_rooms(attendees)
 end
 
 
-
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badges
+    
   end
   assign_rooms(attendees).each do |assignment|
     puts room_assignments
